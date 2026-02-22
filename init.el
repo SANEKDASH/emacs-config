9dfc10e0ad7a29987275a0c9fd1f295fa6266d92
alexdash <dashinsky_alexander92@mail.ru>
2026-02-22 23:44:01 +0300

    init.el: add python features for emacs

--- init.el
+++ init.el
@@ -1,19 +1,61 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; ========
+;; PACKAGES
+;; ========
+
+(setq package-archives'
+      (
+       ("melpa"     . "https://melpa.org/packages/")
+       ("org"       . "http://orgmode.org/elpa/")
+       ("gnu"       . "http://elpa.gnu.org/packages/")
+       )
+      )
+
 (package-initialize)
 
-;; Disable splash screen
-(setq inhibit-startup-screen 1)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-;; Enabling IDO mode.
-(ido-mode 1)
+(eval-when-compile
+  (require 'use-package))
 
-;; Tabulation is 4 spaces.
-(setq-default tab-width 4)
+(unless package-archive-contents
+  (package-refresh-contents))
 
-;; Disabling scroll bar.
-(scroll-bar-mode 0)
+(use-package paredit
+  :ensure t
+  :hook ((emacs-lisp-mode lisp-mode) . enable-paredit-mode))
 
+(use-package company
+  :ensure t
+  :config (global-company-mode 1))
+
+(use-package elpy
+  :ensure t
+  :init
+  (advice-add 'python-mode :before (lambda (&rest _) (elpy-enable)))
+  :config
+  (elpy-enable)
+  (setq python-shell-interpreter "/home/alexdash/.local/bin/ipython"
+	python-shell-interpreter-args "-i --simple-prompt")
+  )
+
+;; ======
+;; EDITOR
+;; ======
+
+;; Disable backup files creation
+(setq make-backup-files nil)
+
+;; Disable autosave
+(setq auto-save-default nil)
+
+;; Default encoding - utf-8
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(setq locale-coding-system 'utf-8)
 
 ;; Enable line numbers.
 (global-display-line-numbers-mode)
@@ -25,66 +67,80 @@
                     :background "#4A4A4A"
                     :foreground "#FFFFFF")
 
-
-;; Disabling stuff in the top of the screen
+;; Disabling stuff at the top of the screen
 (tool-bar-mode 0)
 (menu-bar-mode 0)
+
+;; Disabling scroll bar.
+(scroll-bar-mode 0)
+
+;; Disable splash screen
+(setq inhibit-startup-screen 1)
+
+;; Enable visible bell
+(setq visible-bell t)
+
+;; Completely ignore the bell function
+(setq ring-bell-function 'ignore)
+
+;; Enabling IDO mode.
+(ido-mode 1)
+
+;; Options for C files
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (setq c-basic-offset 8)
+            (setq indent-tabs-mode t)
+            (setq tab-width 8)
+	    ))
 
 ;; Scroll margin
 (setq scroll-margin 10)
 (setq scroll-conservatively 101)
 
-
-(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
-      backup-by-copying t     ; Don't delink hardlinks
-      version-control t       ; Use version numbers on backups
-      delete-old-versions t   ; Automatically delete excess backups
-      kept-new-versions 20    ; how many of the newest versions to keep
-      kept-old-versions 5)    ; and how many of the old
-
-
 ;; Keybind to switch windows
 (global-set-key (kbd "M-o") 'other-window)
-
-;; Disable fucking ring bell
-(setq ring-bell-function 'ignore)
 
 ;; Disable line truncate
 (setq-default truncate-lines t)
 
+;; =============
+;; SPELL CHECKER
+;; =============
+
+;; Use hunspell for spell checking.
+(setq ispell-program-name "/usr/bin/hunspell")
+;; Use english dictionary by default
+(setq ispell-dictionary "en_A")
+(defun add-flyspell-hook (&rest modes)
+  (mapc (lambda (mode)
+	  (add-hook mode 'flyspell-mode)
+	  )
+	modes
+	)
+  )
+
+;; Add flyspell hooks to different
+;; text editing modes.
+(add-flyspell-hook
+ 'text-mode-hook
+ 'markdown-mode-hook
+ 'org-mode-hook
+ 'git-commit-mode-hook
+ 'c-mode-common-hook
+ )
+
+;; Delete trailing spaces after saving.
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
- '(custom-enabled-themes '(wheatgrass))
- '(ispell-dictionary nil)
- '(package-selected-packages '(company)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-;; use hunspell as a spell checker
-(setq ispell-program-name "hunspell")
-(setq ispell-dictionary "ru_RU") 
-
-;; use flyspell in text mode
-(add-hook 'text-mode-hook #'flyspell-mode)
-(add-hook 'org-mode-hook  #'flyspell-mode)
-(add-hook 'latex-mode-hook #'flyspell-mode)
-
-
-;; mind M-t to swicth between english and russian dictionaries
-(defun my-flyspell-toggle-dictionary ()
-  (interactive)
-  (if (string= ispell-current-dictionary "ru_RU")
-      (ispell-change-dictionary "en_US")
-    (ispell-change-dictionary "ru_RU")))
-(global-set-key (kbd "M-t") 'my-flyspell-toggle-dictionary)
