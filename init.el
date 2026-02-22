284c738c3f609ae706a519c263fc1803d5c68785
alexdash <dashinsky_alexander92@mail.ru>
2026-02-23 00:18:57 +0300

    init.el: add automatic refresh of changed files

--- init.el
+++ init.el
@@ -104,6 +104,10 @@
 ;; Disable line truncate
 (setq-default truncate-lines t)
 
+;; Automatically refresh changed files
+(global-auto-revert-mode 1)
+(setq global-auto-revert-none-file-buffers t)
+
 ;; =============
 ;; SPELL CHECKER
 ;; =============
@@ -137,7 +141,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages '(magit use-package rainbow-delimiters paredit elpy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
